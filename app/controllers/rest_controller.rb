b990c2ec0fe245ed6abd82731ade16cbc2e2d294
nobelium <me@vignesh.info>
2013-07-13 19:02:58 +0530

    adding controller

---
+++ app/controllers/rest_controller.rb
@@ -0,0 +1,2 @@
+class RestController < ApplicationController
+end
---
+++ test/functional/rest_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RestControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
