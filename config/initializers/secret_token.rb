13256c57906b110745959a163bfcd73ce40c990e
Shiva Nandan <seekshiva@gmail.com>
2013-07-13 19:15:13 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitfeed::Application.config.secret_token = '5ed944560f41f722aaadd0751819772b882f67167415710ab321311513f0018bbb94acf518eb6ae6df03f1998b1864f94e4b22e01cff27eaa0a03b9c5445d0cd'
